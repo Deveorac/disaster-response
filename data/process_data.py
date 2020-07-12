44c939dcd4f204ac2cda3478eca863b42a13b0df
Mehrnaz <50058459+Deveorac@users.noreply.github.com>
2020-07-11 19:28:46 -0700

    Completed file upload

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,92 @@
-import sys
+# Updated July 11, 2020
 
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    """ 
+    Load the data and merge the two datasets 
+    
+    Input: messages_filepath and categories_filepath as strings
+    
+    Output: df dataframe with merged content of the two datasets
+    """
+    df_message = pd.read_csv(messages_filepath)
+    df_category = pd.read_csv(categories_filepath)
+    
+    # merge
+    df = df_message.merge(df_category, on=['id'])
+    
+    print('success')
+    
+    return df
 
 def clean_data(df):
-    pass
+    """
+    Cleans the newly merged dataset by creating category columns, 
+    removing an unnecessary, empty category column (child_alone), 
+    removing incorrect related values, and removing duplicates
+    
+    Input: df dataframe with merged content of messagse and categories
+    
+    Output: df dataframe that is cleaned
+    """
+    # split categories into columns
+    categories = df['categories'].str.split(';', expand=True)
+    
+    # create list of category names
+    row1 = categories.iloc[0]
+    names = row1.transform(lambda x: x[:-2]).tolist()
+    
+    # use these names as new column names
+    categories.columns = names
+    
+    # convert the -1 or -0 in each entry to 1 or 0 
+    for i in categories:
+        # final character is the number we want to keep
+        categories[i] = categories[i].transform(lambda x: x[-1:])
+        
+        # conversion to numeric
+        categories[i] = pd.to_numeric(categories[i])
+        
+    # the related column has some entries with value 2, which represent
+    # incorreect data and should be removed
+    categories = categories[categories['related'] != 2]
+    
+    # we also see that child_alone is always 0, so that column can
+    # be dropped as it never applies
+    categories.drop('child_alone', axis=1, inplace = True)
+    
+    # we can now combine this dataset with the original one 
+    # in the process, we can drop unneeded columns: original and categories
+
+    df.drop('categories', axis=1, inplace = True)
+    df.drop('original', axis=1, inplace=True)
+    
+    df_clean = pd.concat([df, categories], axis=1)
+    
+    # finally we can drop duplicated rows (there are 170)
+    
+    df_clean.drop_duplicates(inplace=True)
+    
+    df = df_clean
+    
+    return df
+    
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save the resultant clean file into SQLite database
+    
+    Input: df cleaned dataframe
+           database_filename string filename for output
+           
+    Output: none
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql("Messages", engine, index=False, if_exists='replace')  
 
 
 def main():
@@ -41,3 +117,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
